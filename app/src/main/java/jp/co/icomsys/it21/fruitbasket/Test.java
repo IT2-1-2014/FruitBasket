1f2d94086ab22aa7ee555b7f8526529b911105b5
yuka <yuka-kato@icomsys.co.jp>
2015-01-24 11:42:17 +0900

    Test.Java activity_test_main.xml を追加

---
+++ app/src/main/java/jp/co/icomsys/it21/fruitbasket/Test.java
@@ -0,0 +1,16 @@
+package jp.co.icomsys.it21.fruitbasket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yuka on 2015/01/24.
+ */
+public class Test extends Activity {
+
+   protected void onCreate(Bundle saveInstanceState){
+       super.onCreate(saveInstanceState);
+       setContentView(R.layout.activity_test_main);
+   }
+
+}
