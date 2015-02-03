0110d4acd68cb554d0c4f2b61919e57ac0e75e5e
Masaharu Hayashi <hayashi.masaharu@gmail.com>
2015-02-04 08:52:06 +0900

    エラーハンドラのクラス（柄だけ）

---
+++ app/src/main/java/jp/co/icomsys/it21/fruitbasket/FBUncaughtExceptionHandler.java
@@ -0,0 +1,11 @@
+package jp.co.icomsys.it21.fruitbasket;
+
+/**
+ * Created by masaharu on 15/02/04.
+ */
+public class FBUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler{
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+
+    }
+}
