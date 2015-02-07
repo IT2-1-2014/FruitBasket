d1a3f3d9feae185b1c79c4c43919e7f2f508e6df
Masaharu Hayashi <hayashi.masaharu@gmail.com>
2015-02-07 15:10:36 +0900

    エラーハンドリング実装（未実行）

--- app/src/main/java/jp/co/icomsys/it21/fruitbasket/FBUncaughtExceptionHandler.java
+++ app/src/main/java/jp/co/icomsys/it21/fruitbasket/FBUncaughtExceptionHandler.java
@@ -1,11 +1,61 @@
 package jp.co.icomsys.it21.fruitbasket;
 
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 /**
- * Created by masaharu on 15/02/04.
+ * 未キャッチな例外のハンドリングクラス.
+ * ハンドリングした情報はSDカードに出力
  */
-public class FBUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler{
+public class FBUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+    private static Context sContext = null;
+
+    private static File BUG_REPORT_FILE = null;
+
+    static {
+        String sdcard = Environment.getExternalStorageDirectory().getPath();
+        String path = sdcard + File.separator + "bug.txt";
+        BUG_REPORT_FILE = new File(path);
+    }
+
+    public FBUncaughtExceptionHandler(Context context) {
+        sContext = context;
+    }
+
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
+        try {
+            saveBugReport(ex);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     *
+     */
+    private void saveBugReport(Throwable th) throws FileNotFoundException {
+        StackTraceElement[] stacks = th.getStackTrace();
+        File file = BUG_REPORT_FILE;
+        PrintWriter pw;
+        pw = new PrintWriter(new FileOutputStream(file));
+
+        StringBuilder sb = new StringBuilder();
+        for (StackTraceElement stack : stacks) {
+            sb.setLength(0);
+            // <class name>#<method name>:<line number>
+            sb.append(stack.getClassName()).append("#");
+            sb.append(stack.getMethodName()).append(":");
+            sb.append(stack.getLineNumber());
+            pw.println(sb.toString());
+        }
+        pw.close();
 
     }
 }
