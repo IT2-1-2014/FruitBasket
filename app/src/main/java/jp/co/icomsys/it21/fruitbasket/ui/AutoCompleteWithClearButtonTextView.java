c8a5740b059ebd6a9ab50914eaf91a76eeff96ec
Masaharu Hayashi <hayashi.masaharu@gmail.com>
2015-02-24 08:30:53 +0900

    登録画面の実装中

--- app/src/main/java/jp/co/icomsys/it21/fruitbasket/ui/AutoCompleteWithClearButtonTextView.java
+++ app/src/main/java/jp/co/icomsys/it21/fruitbasket/ui/AutoCompleteWithClearButtonTextView.java
@@ -5,7 +5,6 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
@@ -14,8 +13,8 @@ import jp.co.icomsys.it21.fruitbasket.R;
 /**
  * AutoCompleteTextView with Clear Button.
  */
-public class AutoCompleteWithClearButtonTextView extends LinearLayout implements View.OnClickListener{
-    
+public class AutoCompleteWithClearButtonTextView extends LinearLayout implements View.OnClickListener {
+
     private AutoCompleteTextView mTextView;
     private ImageButton mClearButton;
 
@@ -43,224 +42,16 @@ public class AutoCompleteWithClearButtonTextView extends LinearLayout implements
         AutoCompleteWithClearButtonTextView a = (AutoCompleteWithClearButtonTextView) li.inflate(R.layout.viewgroup_single_item_input, this);
         mTextView = (AutoCompleteTextView) a.findViewById(R.id.textView);
         mClearButton = (ImageButton) a.findViewById(R.id.clearButton);
-
+        
+        //this.removeAllViews();
+        //this.addView(mTextView);
+        //this.addView(mClearButton);
     }
 
     @Override
     public void onClick(View v) {
-        if( v == mClearButton) {
+        if (v == mClearButton) {
             mTextView.setText("");
         }
     }
 }
-
-//    public AutoCompleteWithClearButtonTextView(Context context) {
-//        this(context,null);
-//    }
-//
-//    public AutoCompleteWithClearButtonTextView(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AutoCompleteWithClearButtonTextView);
-//
-//        try {
-//            // このViewのスタイル定義（a）から値を取得し、メンバ変数にセットする。
-//
-//        } finally {
-//            a.recycle();
-//        }
-//
-//    }
-//    
-//
-//
-//
-//  
-//
-//    @Override
-//    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-//        
-//
-//    }
-//
-//    @Override
-//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-//    }
-//    private String mExampleString; // TODO: use a default from R.string...
-//    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-//    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-//    private Drawable mExampleDrawable;
-//
-//    private TextPaint mTextPaint;
-//    private float mTextWidth;
-//    private float mTextHeight;
-//
-//    public AutoCompleteWithClearButtonTextView(Context context) {
-//        super(context);
-//        init(null, 0);
-//    }
-//
-//    public AutoCompleteWithClearButtonTextView(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//        init(attrs, 0);
-//    }
-//
-//    public AutoCompleteWithClearButtonTextView(Context context, AttributeSet attrs, int defStyle) {
-//        super(context, attrs, defStyle);
-//        init(attrs, defStyle);
-//    }
-//
-//    @Override
-//    protected void onLayout(boolean b, int i, int i2, int i3, int i4) {
-//
-//    }
-//
-//    private void init(AttributeSet attrs, int defStyle) {
-//        // Load attributes
-//        final TypedArray a = getContext().obtainStyledAttributes(
-//                attrs, R.styleable.AutoCompleteWithClearButtonTextView, defStyle, 0);
-//
-//        mExampleString = a.getString(
-//                R.styleable.AutoCompleteWithClearButtonTextView_exampleString);
-//        mExampleColor = a.getColor(
-//                R.styleable.AutoCompleteWithClearButtonTextView_exampleColor,
-//                mExampleColor);
-//        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-//        // values that should fall on pixel boundaries.
-//        mExampleDimension = a.getDimension(
-//                R.styleable.AutoCompleteWithClearButtonTextView_exampleDimension,
-//                mExampleDimension);
-//
-//        if (a.hasValue(R.styleable.AutoCompleteWithClearButtonTextView_exampleDrawable)) {
-//            mExampleDrawable = a.getDrawable(
-//                    R.styleable.AutoCompleteWithClearButtonTextView_exampleDrawable);
-//            mExampleDrawable.setCallback(this);
-//        }
-//
-//        a.recycle();
-//
-//        // Set up a default TextPaint object
-//        mTextPaint = new TextPaint();
-//        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-//        mTextPaint.setTextAlign(Paint.Align.LEFT);
-//
-//        // Update TextPaint and text measurements from attributes
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    private void invalidateTextPaintAndMeasurements() {
-//        mTextPaint.setTextSize(mExampleDimension);
-//        mTextPaint.setColor(mExampleColor);
-//        mTextWidth = mTextPaint.measureText(mExampleString);
-//
-//        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-//        mTextHeight = fontMetrics.bottom;
-//    }
-//
-//    @Override
-//    protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-//
-//        // TODO: consider storing these as member variables to reduce
-//        // allocations per draw cycle.
-//        int paddingLeft = getPaddingLeft();
-//        int paddingTop = getPaddingTop();
-//        int paddingRight = getPaddingRight();
-//        int paddingBottom = getPaddingBottom();
-//
-//        int contentWidth = getWidth() - paddingLeft - paddingRight;
-//        int contentHeight = getHeight() - paddingTop - paddingBottom;
-//
-//        // Draw the text.
-//        canvas.drawText(mExampleString,
-//                paddingLeft + (contentWidth - mTextWidth) / 2,
-//                paddingTop + (contentHeight + mTextHeight) / 2,
-//                mTextPaint);
-//
-//        // Draw the example drawable on top of the text.
-//        if (mExampleDrawable != null) {
-//            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-//                    paddingLeft + contentWidth, paddingTop + contentHeight);
-//            mExampleDrawable.draw(canvas);
-//        }
-//    }
-//
-//    /**
-//     * Gets the example string attribute value.
-//     *
-//     * @return The example string attribute value.
-//     */
-//    public String getExampleString() {
-//        return mExampleString;
-//    }
-//
-//    /**
-//     * Sets the view's example string attribute value. In the example view, this string
-//     * is the text to draw.
-//     *
-//     * @param exampleString The example string attribute value to use.
-//     */
-//    public void setExampleString(String exampleString) {
-//        mExampleString = exampleString;
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    /**
-//     * Gets the example color attribute value.
-//     *
-//     * @return The example color attribute value.
-//     */
-//    public int getExampleColor() {
-//        return mExampleColor;
-//    }
-//
-//    /**
-//     * Sets the view's example color attribute value. In the example view, this color
-//     * is the font color.
-//     *
-//     * @param exampleColor The example color attribute value to use.
-//     */
-//    public void setExampleColor(int exampleColor) {
-//        mExampleColor = exampleColor;
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    /**
-//     * Gets the example dimension attribute value.
-//     *
-//     * @return The example dimension attribute value.
-//     */
-//    public float getExampleDimension() {
-//        return mExampleDimension;
-//    }
-//
-//    /**
-//     * Sets the view's example dimension attribute value. In the example view, this dimension
-//     * is the font size.
-//     *
-//     * @param exampleDimension The example dimension attribute value to use.
-//     */
-//    public void setExampleDimension(float exampleDimension) {
-//        mExampleDimension = exampleDimension;
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    /**
-//     * Gets the example drawable attribute value.
-//     *
-//     * @return The example drawable attribute value.
-//     */
-//    public Drawable getExampleDrawable() {
-//        return mExampleDrawable;
-//    }
-//
-//    /**
-//     * Sets the view's example drawable attribute value. In the example view, this drawable is
-//     * drawn above the text.
-//     *
-//     * @param exampleDrawable The example drawable attribute value to use.
-//     */
-//    public void setExampleDrawable(Drawable exampleDrawable) {
-//        mExampleDrawable = exampleDrawable;
-//    }
-//}
